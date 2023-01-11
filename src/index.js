f385201425a1e8cb25592167adddd69f8a5ccd54
OksanaTryhub <ksanatrigub@gmail.com>
2023-01-11 17:43:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
-import { App } from 'components/App';
+// import { App } from 'components/App';
 import './index.css';
 
 // ReactDOM.createRoot(document.getElementById('root')).render(
